d252e584ca2e17219115ceb0009ae27e62110553
Michel-Gomes33 <michel.lrgomes@outlook.com>
2025-04-08 12:04:34 -0400

    succesful localhost and riddle filter

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,19 @@
-import app from './app';
+import express from 'express';
+import dotenv from 'dotenv';
+import router from '../src/routes/riddleRoute' ;
 
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => {
+  res.send('Server is working!');
+});
+
+
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- server/src/types/types.ts
+++
@@ -1,6 +0,0 @@
-// types/riddle.d.ts
-export interface Riddle {
-    question: string;
-    answer: string;
-  }
-  
