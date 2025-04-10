48b51726c215825a1b47198c7542c8334ad5cf55
tiffany clark <tiffanymarie9811@gmail.com>
2025-04-10 16:09:54 -0400

    backtracked to get locally working again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     __DEFINES__: JSON.stringify({
-      VITE_API_URL: process.env.VITE_API_URL
-    })
-  }
-})
+      VITE_API_URL: process.env.VITE_API_URL,
+    }),
+  },
+});
