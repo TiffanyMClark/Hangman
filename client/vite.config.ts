6c071d73d87c3dad3233560de73b657098df754c
chicalauren <lambertelauren@gmail.com>
2025-04-10 10:18:27 -0500

    Revert "vite config"
    This reverts commit e99d62367850f096b0f8cc61c0c337555f388c37.
    
    revert commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-  },
   define: {
     __DEFINES__: JSON.stringify({
       VITE_API_URL: process.env.VITE_API_URL
     })
-  },
-});
+  }
+})
